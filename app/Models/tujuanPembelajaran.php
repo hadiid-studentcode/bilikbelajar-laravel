9e255cfba24f869cd4a69b02994d58e2caed03d5
hadiid-studentcode <hadiidandri2000@gmail.com>
2025-04-16 14:21:06 +0700

    menambahkan tabel cpdantp

---
+++ app/Models/tujuanPembelajaran.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tujuanPembelajaran extends Model
+{
+    /** @use HasFactory<\Database\Factories\TujuanPembelajaranFactory> */
+    use HasFactory;
+}
