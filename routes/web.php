0602d3dc74daada577f1d472ed7e99ec62892a5d
hadiid-studentcode <hadiidandri2000@gmail.com>
2025-04-19 10:24:16 +0700

    tampilan login

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 // Route::get('/404', function () {
 //     return view('404');
