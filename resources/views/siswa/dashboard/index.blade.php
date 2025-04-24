cb20612122b57c34aaf63bcb878dbd6eebb70b7d
hadiid-studentcode <hadiidandri2000@gmail.com>
2025-04-24 21:47:02 +0700

    update

--- resources/views/siswa/dashboard/index.blade.php
+++ resources/views/siswa/dashboard/index.blade.php
@@ -228,7 +228,7 @@
                             </div>
                         @endif
 
-                        <div class="row row-cols-1 row-cols-md-4 g-4 mb-5">
+                        <div class="row row-cols-1 row-cols-md-4 g-3 mb-5">
 
                             @foreach ($materi as $m)
                                 <div class="col">
