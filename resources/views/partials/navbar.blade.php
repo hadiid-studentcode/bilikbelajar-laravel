321725740686b93e703049f575f5dc19b7123eae
hadiid-studentcode <hadiidandri2000@gmail.com>
2025-04-28 10:53:46 +0700

    avatar images guru

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -33,20 +33,34 @@
               <!-- User -->
               <li class="nav-item navbar-dropdown dropdown-user dropdown">
                   <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                      <div class="avatar avatar-online">
-                          <img src="{{ asset('assets/img/avatars/1.png') }}" alt
-                              class="w-px-40 h-auto rounded-circle" />
-                      </div>
+                    <div class="avatar avatar-online">
+                        @if (session()->has('siswa'))
+                            <span class="avatar-initial rounded-circle bg-primary">
+                                {{ substr(strtoupper(session()->get('siswa')->nama), 0, 2) }}
+                            </span>
+                        @else
+                            <span class="avatar-initial rounded-circle bg-primary">
+                                {{ substr(strtoupper(Auth::user()->username), 0, 2) }}
+                            </span>
+                        @endif
+                    </div>
                   </a>
                   <ul class="dropdown-menu dropdown-menu-end">
                       <li>
                           <a class="dropdown-item" href="#">
                               <div class="d-flex">
                                   <div class="flex-shrink-0 me-3">
-                                      <div class="avatar avatar-online">
-                                          <img src="{{ asset('assets/img/avatars/1.png') }}" alt
-                                              class="w-px-40 h-auto rounded-circle" />
-                                      </div>
+                                    <div class="avatar avatar-online">
+                                        @if (session()->has('siswa'))
+                                            <span class="avatar-initial rounded-circle bg-primary">
+                                                {{ substr(strtoupper(session()->get('siswa')->nama), 0, 2) }}
+                                            </span>
+                                        @else
+                                            <span class="avatar-initial rounded-circle bg-primary">
+                                                {{ substr(strtoupper(Auth::user()->username), 0, 2) }}
+                                            </span>
+                                        @endif
+                                    </div>
                                   </div>
                                   <div class="flex-grow-1">
                                       <span class="fw-semibold d-block">
