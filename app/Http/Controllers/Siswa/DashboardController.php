53508645c520a47b2db6fcabd0c2d144b5b0291c
hadiid-studentcode <hadiidandri2000@gmail.com>
2025-05-14 05:35:11 +0700

    'bug'

--- app/Http/Controllers/Siswa/DashboardController.php
+++ app/Http/Controllers/Siswa/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Siswa;
 
 use App\Http\Controllers\Controller;
-use App\Models\CapaianPembelajaran;
+use App\Models\capaianPembelajaran;
 use App\Models\Materi;
 
 class DashboardController extends Controller
@@ -20,7 +20,7 @@ class DashboardController extends Controller
         }
 
         $materi = Materi::select('id', 'nama', 'kelas', 'image')->where('kelas', session()->get('siswa')->kelas)->get();
-        $capaianPembelajaran = CapaianPembelajaran::where('kelas', session()->get('siswa')->kelas)->first();
+        $capaianPembelajaran = capaianPembelajaran::where('kelas', session()->get('siswa')->kelas)->first();
 
         return view('siswa.dashboard.index', compact('materi', 'capaianPembelajaran', 'title'));
     }
--- app/Models/tujuanPembelajaran.php
+++ app/Models/tujuanPembelajaran.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TujuanPembelajaran extends Model
+class tujuanPembelajaran extends Model
 {
     /** @use HasFactory<\Database\Factories\TujuanPembelajaranFactory> */
     use HasFactory;
